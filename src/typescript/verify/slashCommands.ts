86bfaa8545415d22bfac5184f5b807cff3420af1
drowning14 <drowning.dev1@gmail.com>
2025-05-23 18:48:07 +0330

    doing stuff (help)

---
+++ src/typescript/verify/slashCommands.ts
@@ -0,0 +1,29 @@
+import { Routes, REST, RESTPostAPIChatInputApplicationCommandsJSONBody } from "discord.js";
+import { config } from "dotenv";
+import logger from "../logging/logger.js";
+config();
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
+
+async function verifySlashCommands(commands: RESTPostAPIChatInputApplicationCommandsJSONBody[]) {
+  try {
+    console.log("---------------------------------");
+    await logger.info("Registering Slash Commands...");
+    console.log("Registering Slash Commands...");
+
+    // prettier-ignore
+    await rest.put(
+      Routes.applicationCommands(process.env.ClientId!),
+      { body: commands }
+    );
+    await logger.info("Slash Commands Registered Successfully!");
+    console.log("Slash Commands Registered Successfully!");
+  } catch (error) {
+    await logger.error("Registering Slash Commands Failed!");
+    console.error("Registering Slash Commands Failed!");
+    await logger.error(error);
+    process.exit(1);
+  }
+}
+
+export default verifySlashCommands;
