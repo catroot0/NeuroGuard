41cd5034c6beec8de3bef37471dc460a14be4fe4
drowning14 <drowning.dev1@gmail.com>
2025-05-25 17:55:02 +0330

    using dotenv the way the bestPractice says

---
+++ src/typescript/config.ts
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const token = process.env.token;
+export const clientId = process.env.clientId;
+export const clientSecret = process.env.clientSecret;
+export const redirectUrl = process.env.redirectUrl;
