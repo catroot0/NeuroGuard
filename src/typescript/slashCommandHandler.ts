44b6ffba2024678747004e182b0ef881861587a0
drowning14 <drowning.dev1@gmail.com>
2025-05-29 20:45:07 +0330

    updating slash command handler

--- src/typescript/slashCommandHandler.ts
+++ src/typescript/slashCommandHandler.ts
@@ -1,8 +1,11 @@
 import { ChatInputCommandInteraction } from "discord.js";
+import verify from "./commands/verify.js";
 
 async function commandHandler(interaction: ChatInputCommandInteraction, commandName: string) {
   if (commandName === "echo") {
     await interaction.reply(`${interaction.options.getString("input")}`);
+  } else if (commandName === "verify") {
+    await verify(interaction);
   }
 }
 
