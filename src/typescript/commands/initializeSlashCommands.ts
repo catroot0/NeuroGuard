ac55f0dbe52d0382879ff9fa774bd66f67673c4f
drowningdev <drowning.dev1@gmail.com>
2025-07-01 18:16:54 +0330

    adding some stuff. not sure

--- src/typescript/commands/initializeSlashCommands.ts
+++ src/typescript/commands/initializeSlashCommands.ts
@@ -1,21 +1,43 @@
-import { RESTPostAPIChatInputApplicationCommandsJSONBody, SlashCommandBuilder } from "discord.js";
+import { ChannelType, RESTPostAPIChatInputApplicationCommandsJSONBody, SlashCommandBuilder } from "discord.js";
 
 // Returns an array of slash commands to register with Discord
 function initializeSlashCommands(): RESTPostAPIChatInputApplicationCommandsJSONBody[] {
   const rawCommands = [
     new SlashCommandBuilder()
       .setName("echo")
-      .setDescription("Replies with your input.")
+      .setDescription("Repeat back the message you provide.")
       .addStringOption((option) =>
         option
-          .setName("input")
-          .setDescription("The input to echo back.")
+          .setName("message")
+          .setDescription("The message to repeat.")
           .setRequired(true)
       ),
 
     new SlashCommandBuilder()
       .setName("verify")
-      .setDescription("Start the verification process to access the server."),
+      .setDescription("Begin the server verification process."),
+
+    new SlashCommandBuilder()
+      .setName("setup")
+      .setDescription("Configure Neuro Guard for your server.")
+      .addChannelOption(option =>
+        option
+          .setName("verification_channel")
+          .setDescription("Channel where people will use to verify their account.")
+          .setRequired(true)
+      )
+      .addRoleOption(option => 
+        option
+          .setName("member_role")
+          .setDescription("Role assigned to verified members.")
+          .setRequired(true)
+      )
+      .addStringOption(option => 
+        option
+          .setName("appeal_server")
+          .setDescription("Invite link of your appeal server.")
+          .setRequired(true)
+      )
   ];
 
   return rawCommands.map((command) => command.toJSON());
